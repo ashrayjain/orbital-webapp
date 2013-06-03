b639858e3abaff0a15547878fdb3ffd13086d7d2
Ashray Jain <ashrayj11@gmail.com>
2013-06-03 22:00:27 +0300

    Basic home page layout and name change

--- ModMaven/main.py
+++ ModMaven/main.py
@@ -6,10 +6,10 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 class MainPage(webapp2.RequestHandler):
-  """ Handler for the front page."""
-  def get(self):
-      template = jinja_environment.get_template('main.html')
-      self.response.out.write(template.render())
-      
+    """ Handler for the front page."""
+    def get(self):
+        template = jinja_environment.get_template('main.html')
+        self.response.out.write(template.render())
+
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
